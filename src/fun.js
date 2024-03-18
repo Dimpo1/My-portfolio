6c3e69c48112176532d973882591e3828e649641
Dimpo1 <111021090+Dimpo1@users.noreply.github.com>
2024-03-18 03:27:25 +0200

    updating JS

--- src/fun.js
+++ src/fun.js
@@ -1,6 +1,7 @@
 function message() {
   let name = prompt("Please provide your name 🙏🏾");
   alert(`Thank you ${name}! The pages will be loaded soon.😉`);
+  console.log(name);
 }
-let button = document.querySelector("#page");
-button.addEventListener("click", message);
+let page = document.querySelectorAll(".page");
+page.addEventListener("click", message);
